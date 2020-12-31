3fdaf9d814d59b5836cf06c466f59d47ee395f8d
akshay2739 <akshaypatel2739@gmail.com>
2020-12-31 16:03:20 +0530

    video size reduced

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
 
 const Footer = () => {
 	return (
@@ -42,6 +41,7 @@ const Footer = () => {
 				</div>
 
 				<iframe
+					title='map'
 					className='shadow'
 					src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.67345386066!2d-122.48760368437033!3d48.86443697928807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5485ba44f3447de5%3A0xe506691a25db8d5a!2sBellewood%20Farms%20%26%20Distillery!5e0!3m2!1sen!2sin!4v1609408997098!5m2!1sen!2sin'
 					style={{ width: '100%', height: 250 }}
--- front-end/src/components/LandingCarousel.js
+++ front-end/src/components/LandingCarousel.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Col, Row, Image } from 'react-bootstrap'
 
 const LandingCarousel = () => {
 	return (
