283b9ec1c1a99692cb8ffae367505c94792b3e31
akshay2739 <akshaypatel2739@gmail.com>
2021-05-07 02:29:22 +0530

    AWS S3 is added

--- back-end/routes/uploadRoutes.js
+++ back-end/routes/uploadRoutes.js
@@ -1,21 +1,26 @@
 import express from 'express'
 import multer from 'multer'
 import path from 'path'
+import 'colors'
+import aws from 'aws-sdk'
+import multerS3 from 'multer-s3'
+
 const uploadRouter = express.Router()
 
-const storage = multer.diskStorage({
-	destination(req, file, cb) {
-		cb(null, 'uploads/')
-	},
-	filename(req, file, cb) {
-		cb(
-			null,
-			`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-		)
-	},
-})
+// const storage = multer.diskStorage({
+// 	destination(req, file, cb) {
+// 		cb(null, 'uploads/')
+// 	},
+// 	filename(req, file, cb) {
+// 		cb(
+// 			null,
+// 			`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+// 		)
+// 	},
+// })
 
 const checkFileType = (file, cb) => {
+	console.log('CHECK')
 	const fileType = /jpg|jpeg|png/
 	const extname = fileType.test(path.extname(file.originalname).toLowerCase())
 	const mimetype = fileType.test(file.mimetype)
@@ -27,15 +32,69 @@ const checkFileType = (file, cb) => {
 	}
 }
 
+// const upload = multer({
+// 	storage,
+// 	fileFilter: (req, file, cb) => {
+// 		checkFileType(file, cb)
+// 	},
+// })
+
+// uploadRouter.post('/', upload.single('image'), (req, res) => {
+// 	res.send(`/${req.file.path.replace(/\\/g, '/')}`)
+// })
+
+// uploadRouter.post('/', (req, res) => {
+//
+// 	aws.config.region = 'eu-west-1'
+// 	const S3_BUCKET = 'my-shop27'
+
+// 	const S3params = {
+// 		Bucket: 'my-shop27',
+// 		Key: req.fileName,
+// 		Expires: 60,
+// 		ContentType: 'jpeg',
+// 		ACL: 'public-read',
+// 	}
+
+// 	s3.getSignedUrl('putObject', S3params, (err, data) => {
+// 		if (err) {
+// 			console.log(err)
+// 			return res.end()
+// 		}
+
+// 		res.send(`https://my-shop27.s3.amazonaws.com/${req.fileName}`)
+// 	})
+
+// 	// res.send(`/${req.file.path.replace(/\\/g, '/')}`)
+// })
+
+const s3 = new aws.S3({
+	accessKeyId: 'AKIAYIPB5AHSJSHOJC5C',
+	secretAccessKey: '3F31aYzHV2shqFP91LNReUSy8Z+rlBMfEqfKWES1',
+})
+
 const upload = multer({
-	storage,
+	storage: multerS3({
+		s3: s3,
+		bucket: 'my-shop27',
+		acl: 'public-read',
+		metadata: function (req, file, cb) {
+			console.log('CHECK 2')
+			cb(null, { fieldName: file.fieldname })
+		},
+		key: function (req, file, cb) {
+			console.log('CHECK')
+			cb(null, Date.now().toString())
+		},
+	}),
 	fileFilter: (req, file, cb) => {
 		checkFileType(file, cb)
 	},
 })
 
 uploadRouter.post('/', upload.single('image'), (req, res) => {
-	res.send(`/${req.file.path.replace(/\\/g, '/')}`)
+	// res.send(`/${req.file.path.replace(/\\/g, '/')}`)
+	res.send(req.file.location)
 })
 
 export default uploadRouter
